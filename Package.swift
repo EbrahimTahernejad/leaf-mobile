4320fae7bf358fa996e3342bcacbca64861e3236
EbrahimTahernejad <EbrahimTahernejad@users.noreply.github.com>
2023-03-29 17:33:36 +0000

    Leaf Lib Release 1.0.7

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     .target(name: "LeafC", dependencies: ["LibLeaf"], path: "Sources/LeafC"),
     .binaryTarget(
       name: "LibLeaf",
-      url: "https://github.com/EbrahimTahernejad/leaf-mobile/releases/download/1.0.6/LibLeaf.xcframework.zip",
-      checksum: "68326ebf0324f20db0663a138e920a059337f77e6f79cd87915a3b648965aac9"
+      url: "https://github.com/EbrahimTahernejad/leaf-mobile/releases/download/1.0.7/LibLeaf.xcframework.zip",
+      checksum: "2a2a451b9bd2d2b167bc77266ae74d299e6fb56d35d332231da3381ced9e9542"
     )
   ]
 )
